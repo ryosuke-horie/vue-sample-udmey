fae983fd1b8ab6151d7ab19f7c32333055cb35af
ryosuke-horie <ryosuke03140@gmial.com>
2023-11-19 23:10:49 +0900

    inputの値を受け取る

--- vue-sample/src/components/Payment.vue
+++ vue-sample/src/components/Payment.vue
@@ -14,18 +14,25 @@ const url2 = 'yahoo.com'
 const buy = (itemName: string) => {
     alert('Are you sure to buy'+ itemName + '?')
 }
+
+const input = (event:any) => {
+    console.log('event:' +  event.target.value)
+}
 </script>
 
 <template>
     <div class="container">
         <h1>最近の支出</h1>
+        <input v-on:input="input"/>
+
         <div class="payment">
             <label>{{ itemName1 }}</label>
             <label>{{price1}}円</label>
             <a v-bind:href="url1">bought at ...</a>
             <button v-on:click="buy(itemName1)">BUY</button>
         </div>
-            <div class="payment">
+
+        <div class="payment">
             <label>{{ itemName2 }}</label>
             <label>{{price2}}円</label>
             <a v-bind:href="url2">bought at ...</a>
