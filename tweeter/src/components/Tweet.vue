ec25f8e406d3602ab6fbbcd0694128325329c7b0
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-11-20 10:55:35 +0000

    tweetアプリ

---
+++ tweeter/src/components/Tweet.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+const tweets = [{ id: 1, description: "Hello World" }, { id: 2, description: "Hello Vue 3" }];
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <div class="form-container">
+        <input />
+        <button class="save-button">post</button>
+    </div>
+    <div class="tweet-container">
+        <ul>
+            <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+                <span>{{ tweet.description }}</span>
+            </li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+  padding: 24px 0;
+  width: 60%;
+  margin-bottom: 12px;
+  border-radius: 4px;
+}
+
+.tweet-list {
+    list-style: none;
+}
+
+button {
+   color: #fff;
+   font-weight: bold;
+   background-color: aqua;
+   border-radius: 2px;
+   border: none;
+   width: 60px;
+   height: 22px;
+}
+
+button:hover {
+    background-color: aquamarine;
+}
+
+input {
+    margin-bottom: 16px;
+}
+</style>
